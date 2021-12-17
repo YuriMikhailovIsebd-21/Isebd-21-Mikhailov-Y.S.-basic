22dbc1f72588a5e6a28db484dc68165e9068e443
Юрий Михайлов <yuri_2002_q@mail.ru>
2021-12-17 22:18:21 +0400

    Lab2

--- WindowsFormsTeplovoz/ISEbd21_Mikhailov_YS_ Basic_Lab1/Teplovoz.cs
+++ WindowsFormsTeplovoz/ISEbd21_Mikhailov_YS_ Basic_Lab1/Teplovoz.cs
@@ -1,144 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace WindowsFormsTeplovoz
 {
-    class Teplovoz
+    public class Teplovoz : Lokomotiv
     {
-
-        private float _startPosX;
-
-        private float _startPosY;
-
-        private int _pictureWidth;
-
-        private int _pictureHeight;
-
-        private readonly int TeploWidth = 100;
-
-        private readonly int TeploHeight = 30;
-
-        public Color MainColor { private set; get; }
-        public Color WindowColor { private set; get; }
-        public bool DoorTeplo { private set; get; }
-        public bool TubeTeplo { private set; get; }
-        public bool Windows { private set; get; }
-        public bool TeploBody { private set; get; }
-
-        /// <param name="mainColor"></param>
-        /// <param name="window"></param>
-        /// <param name="windows"></param>
-        /// <param name="TeploBody"></param>
-        /// <param name="DoorTeplo"></param>
-        /// <param name="TubeTeplo"></param>
-        public void Init(Color mainColor, Color windowColor, bool windows, bool doorTeplo, bool Teplobody, bool tubeTeplo)
-        {
-            MainColor = mainColor;
-            WindowColor = windowColor;
-            TubeTeplo = tubeTeplo;
-            TeploBody = Teplobody;
-            DoorTeplo = doorTeplo;
-            Windows = windows;
-        }
         /// <summary>
-        /// 
+        /// Дополнительный цвет
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        public void SetPosition(int x, int y, int width, int height)
-        {
-            _startPosX = x;
-            _startPosY = y;
-            _pictureHeight = height;
-            _pictureWidth = width;
-
-        }
+        public Color DopColor { private set; get; }
         /// <summary>
-        /// 
+        /// Признак наличия трубы
         /// </summary>
-        /// <param name="direction"></param>
-        public void MoveTeplo(Direction direction)
-        {
-
-            float step = 20;
-            switch (direction)
-            {
-                case Direction.Right:
-                    if (_startPosX + step < _pictureWidth - TeploWidth)
-                    {
-                        _startPosX += step;
-                    }
-                    break;
-                case Direction.Left:
-                    if (_startPosX - step > 0)
-                    {
-                        _startPosX -= step;
-                    }
-                    break;
-                case Direction.Up:
-                    if (_startPosY - step > 0)
-                    {
-                        _startPosY -= step;
-                    }
-                    break;
-                case Direction.Down:
-                    if (_startPosY + step < _pictureHeight - TeploHeight)
-                    {
-                        _startPosY += step;
-                    }
-                    break;
-            }
-        }
+        public bool Tube { private set; get; }
         /// <summary>
-        /// 
+        /// Признак наличия "дисков"
         /// </summary>
-        /// <param name="g"></param>
-        public void DrawTeplo(Graphics g)
+        public bool Disk { private set; get; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес автомобиля</param>
+
+        /// <param name="mainColor">Основной цвет кузова</param>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="tube"></param>
+        /// <param name="disk"></param>
+
+        public Teplovoz(int maxSpeed, float weight, Color mainColor, Color dopColor,bool tube, bool disk) :
+ base(maxSpeed, weight, mainColor, 320, 115)
+        {
+            DopColor = dopColor;
+            Tube = tube;
+            Disk = disk;      
+        }
+
+        public override void DrawTransport(Graphics g)
         {
             Pen pen = new Pen(Color.Black);
-            //TeploBody:
-            {
+            Brush dopBrush = new SolidBrush(DopColor);
 
-                //body
-                g.DrawRectangle(pen, _startPosX, _startPosY + 15, 150, 20);
-                g.DrawLine(pen, _startPosX, _startPosY + 15, _startPosX + 8, _startPosY);
-                g.DrawLine(pen, _startPosX + 8, _startPosY, _startPosX + 150, _startPosY);
-                g.DrawLine(pen, _startPosX + 150, _startPosY, _startPosX + 150, _startPosY + 18);
-                //dopParts
-                g.FillRectangle(Brushes.Black, _startPosX + 150, _startPosY + 10, 5, 25);
-                g.FillRectangle(Brushes.Black, _startPosX + 10, _startPosY + 35, 45, 15);
-                g.FillRectangle(Brushes.Black, _startPosX + 90, _startPosY + 35, 45, 15);
-                //door
-                g.DrawRectangle(pen, _startPosX + 50, _startPosY + 5, 15, 25);
-                g.FillRectangle(Brushes.White, _startPosX + 50 + 1, _startPosY + 5 + 1, 15, 25);
+            base.DrawTransport(g);
 
-                //wheels
-                g.DrawEllipse(pen, _startPosX + 6, _startPosY + 40, 15, 15);
-                g.DrawEllipse(pen, _startPosX + 35, _startPosY + 40, 15, 15);
-                g.DrawEllipse(pen, _startPosX + 84, _startPosY + 40, 15, 15);
-                g.DrawEllipse(pen, _startPosX + 115, _startPosY + 40, 15, 15);
-
-                //windows
-                g.DrawRectangle(Pens.Blue, _startPosX + 13, _startPosY + 3, 10, 10);
-                g.DrawRectangle(Pens.Blue, _startPosX + 33, _startPosY + 3, 10, 10);
-                g.DrawRectangle(Pens.Blue, _startPosX + 130, _startPosY + 3, 10, 10); 
-
-
-            }
-            if (TubeTeplo)
+            if (Tube)
             {
                 g.DrawLine(pen, _startPosX + 90, _startPosY - 12, _startPosX + 90, _startPosY + 0);
                 g.DrawLine(pen, _startPosX + 80, _startPosY - 12, _startPosX + 80, _startPosY + 0);
                 g.DrawLine(pen, _startPosX + 80, _startPosY - 12, _startPosX + 90, _startPosY - 12);
             }
 
-
+            if (Disk)
+            {
+                g.DrawEllipse(pen, _startPosX + 6, _startPosY + 40, 15, 15);
+                g.DrawEllipse(pen, _startPosX + 35, _startPosY + 40, 15, 15);
+                g.DrawEllipse(pen, _startPosX + 84, _startPosY + 40, 15, 15);
+                g.DrawEllipse(pen, _startPosX + 115, _startPosY + 40, 15, 15);
+                g.DrawLine(pen, _startPosX + 42, _startPosY + 40, _startPosX + 42, _startPosY + 55);
+                g.DrawLine(pen, _startPosX + 13, _startPosY + 40, _startPosX + 13, _startPosY + 55);
+                g.DrawLine(pen, _startPosX + 92, _startPosY + 40, _startPosX + 92, _startPosY + 55);
+                g.DrawLine(pen, _startPosX + 123, _startPosY + 40, _startPosX + 123, _startPosY + 55);
+            }
+            
         }
     }
 }
+
+ 
